64d845f298d593d49c0cf3a36a4f21f86a3f95e1
pawanvirsingh <pawanvir.singh26@gmail.com>
2016-08-20 22:47:48 +0530

    multiple component

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 
 class App extends React.Component {
    render() {
@@ -7,6 +7,43 @@ class App extends React.Component {
             <h1>Header</h1>
             <h2>Content</h2>
             <p>This is the content!!!</p>
+         </div>
+      );
+   }
+}
+
+export default App;
+Multi line comment...*/}
+
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
          </div>
       );
    }
